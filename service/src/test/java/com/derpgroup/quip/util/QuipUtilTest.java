2d7c997fbc80c43d3ab3105805d2117c6133f087
unknown <chiaco@gmail.com>
2015-12-02 22:27:24 -0800

    Added a quip substitution function to QuipUtil

--- service/src/test/java/com/derpgroup/quip/util/QuipUtilTest.java
+++ service/src/test/java/com/derpgroup/quip/util/QuipUtilTest.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import org.junit.Test;
 
 import com.derpgroup.derpwizard.voice.exception.DerpwizardException;
+import com.derpgroup.quip.model.Quip;
 
 public class QuipUtilTest {
 
@@ -15,6 +16,12 @@ public class QuipUtilTest {
   public void testContentSubstitution_noValues() throws DerpwizardException{
     Map<String, String> replacementValues = new HashMap<String, String>();
     String response = QuipUtil.substituteContent("Is that really your name?", replacementValues);
+    assertEquals("Is that really your name?",response);
+  }
+  
+  @Test
+  public void testContentSubstitution_null() throws DerpwizardException{
+    String response = QuipUtil.substituteContent("Is that really your name?", null);
     assertEquals("Is that really your name?",response);
   }
   
@@ -96,6 +103,104 @@ public class QuipUtilTest {
 
     String response = QuipUtil.substituteContent("Who's awesome? [NAME]", replacementValues);
     assertEquals("Who's awesome? CompliBot",response);
+  }
+  
+  public Quip getBasicQuip(){
+    Quip quip = new Quip();
+    quip.setQuipGroup("I_LIGHT_UP");
+    quip.setTags(null);
+    quip.setUsageRules(null);
+    quip.setText("I light up every time you talk to me!");
+    quip.setSsml("I light up every time you talk to me!");
+    quip.setTargetableText("I light up every time [TARGET] talks to me!");
+    quip.setTargetableSsml("I light up every time [TARGET] talks to me!");
+    return quip;
+  }
+  
+  public Quip getAdvancedQuip(){
+    Quip quip = new Quip();
+    quip.setQuipGroup("STOCKHOLME");
+    quip.setTags(null);
+    quip.setUsageRules(null);
+    quip.setText("Stockholme Syndrome is the only reason CompliBot likes you.");
+    quip.setSsml("Stockholme Syndrome is the only reason [CompliBot] likes you.");
+    quip.setTargetableText("Stockholme Syndrome is the only reason CompliBot likes [TARGET].");
+    quip.setTargetableSsml("Stockholme Syndrome is the only reason [CompliBot] likes [TARGET].");
+    return quip;
+  }
+  
+  @Test
+  public void testQuipSubstitution_bothNull(){
+    Quip quip = getBasicQuip();
+    Quip subbedQuip = QuipUtil.substituteContent(quip, null, null);
+    assertEquals("I_LIGHT_UP",subbedQuip.getQuipGroup());
+    assertEquals(null,subbedQuip.getTags());
+    assertEquals(null,subbedQuip.getUsageRules());
+    assertEquals("I light up every time you talk to me!",subbedQuip.getText());
+    assertEquals("I light up every time you talk to me!",subbedQuip.getSsml());
+    assertEquals("I light up every time [TARGET] talks to me!",subbedQuip.getTargetableText());
+    assertEquals("I light up every time [TARGET] talks to me!",subbedQuip.getTargetableSsml());
+  }
+  
+  @Test
+  public void testQuipSubstitution_phoneticsNull(){
+    Quip quip = getBasicQuip();
+    
+    Map<String, String> replacementValues = new HashMap<String, String>();
+    replacementValues.put("[NAME]", "User 1");
+    replacementValues.put("[GENDER_PRONOUN]", "his");
+    replacementValues.put("[TARGET]", "User 2");
+    
+    Quip subbedQuip = QuipUtil.substituteContent(quip, null, replacementValues);
+    assertEquals("I_LIGHT_UP",subbedQuip.getQuipGroup());
+    assertEquals(null,subbedQuip.getTags());
+    assertEquals(null,subbedQuip.getUsageRules());
+    assertEquals("I light up every time you talk to me!",subbedQuip.getText());
+    assertEquals("I light up every time you talk to me!",subbedQuip.getSsml());
+    assertEquals("I light up every time User 2 talks to me!",subbedQuip.getTargetableText());
+    assertEquals("I light up every time User 2 talks to me!",subbedQuip.getTargetableSsml());
+  }
+  
+  @Test
+  public void testQuipSubstitution_normalReplacementsNull(){
+    Quip quip = getBasicQuip();
+    
+    Map<String, String> phoneticReplacements = new HashMap<String, String>();
+    phoneticReplacements.put("[CompliBot]", "<phoneme alphabet=\"ipa\" ph=\"kɒmpləbɑt\"> CompliBot </phoneme>");
+    phoneticReplacements.put("[InsultiBot]", "insultabot");
+    
+    Quip subbedQuip = QuipUtil.substituteContent(quip, phoneticReplacements, null);
+    assertEquals("I_LIGHT_UP",subbedQuip.getQuipGroup());
+    assertEquals(null,subbedQuip.getTags());
+    assertEquals(null,subbedQuip.getUsageRules());
+    assertEquals("I light up every time you talk to me!",subbedQuip.getText());
+    assertEquals("I light up every time you talk to me!",subbedQuip.getSsml());
+    assertEquals("I light up every time [TARGET] talks to me!",subbedQuip.getTargetableText());
+    assertEquals("I light up every time [TARGET] talks to me!",subbedQuip.getTargetableSsml());
+  }
+  
+  @Test
+  public void testQuipSubstitution_normalReplacements(){
+    Quip quip = getAdvancedQuip();
+    
+    Map<String, String> phoneticReplacements = new HashMap<String, String>();
+    phoneticReplacements.put("[CompliBot]", "<phoneme alphabet=\"ipa\" ph=\"kɒmpləbɑt\"> CompliBot </phoneme>");
+    phoneticReplacements.put("[InsultiBot]", "insultabot");
+    
+    Map<String, String> replacementValues = new HashMap<String, String>();
+    replacementValues.put("[GENDER_POSSESSIVE_PRONOUN]", "his");
+    replacementValues.put("[GENDER_OBJECT_PRONOUN]", "him");
+    replacementValues.put("[GENDER_SUBJECT_PRONOUN]", "he");
+    replacementValues.put("[TARGET]", "TargetName");
+    
+    Quip subbedQuip = QuipUtil.substituteContent(quip, phoneticReplacements, replacementValues);
+    assertEquals("STOCKHOLME",subbedQuip.getQuipGroup());
+    assertEquals(null,subbedQuip.getTags());
+    assertEquals(null,subbedQuip.getUsageRules());
+    assertEquals("Stockholme Syndrome is the only reason CompliBot likes you.",subbedQuip.getText());
+    assertEquals("Stockholme Syndrome is the only reason <phoneme alphabet=\"ipa\" ph=\"kɒmpləbɑt\"> CompliBot </phoneme> likes you.",subbedQuip.getSsml());
+    assertEquals("Stockholme Syndrome is the only reason CompliBot likes TargetName.",subbedQuip.getTargetableText());
+    assertEquals("Stockholme Syndrome is the only reason <phoneme alphabet=\"ipa\" ph=\"kɒmpləbɑt\"> CompliBot </phoneme> likes TargetName.",subbedQuip.getTargetableSsml());
   }
 
 }
